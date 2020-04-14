d8c1439dc1c6d10b9f4514b01d72e188dc0c7fc4
Sergey Shibaev <srgjjshibaev@rambler.ru>
2020-04-14 12:50:44 +0300

    Верстка мобильной версии всех страниц без отработки по сталгайду

--- source/js/nav-menu.js
+++ source/js/nav-menu.js
@@ -2,10 +2,10 @@ var button = document.querySelector(".header-nav__close");
 var mainNav = document.querySelector(".page-header__main-nav");
 
 button.classList.add("header-nav__menu");
-mainNav.classList.add("visually-hidden");
+mainNav.classList.add("page-header__main-nav--close");
 
 button.addEventListener("click", function(evt) {
   button.classList.toggle("header-nav__menu");
-  mainNav.classList.toggle("visually-hidden");
+  mainNav.classList.toggle("page-header__main-nav--close");
 });
 
