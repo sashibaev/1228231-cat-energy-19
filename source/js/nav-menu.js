7b554abd59bbbdc1d93277750594ead09ba1515e
Sergey Shibaev <srgjjshibaev@rambler.ru>
2020-04-17 16:23:41 +0300

    Убрал видимость кнопки меню закрыть в мобильной версии без js

--- source/js/nav-menu.js
+++ source/js/nav-menu.js
@@ -1,6 +1,7 @@
 var button = document.querySelector(".header-nav__close");
 var mainNav = document.querySelector(".page-header__main-nav");
 
+button.classList.remove("header-nav__close--hidden");
 button.classList.add("header-nav__menu");
 mainNav.classList.add("page-header__main-nav--close");
 
