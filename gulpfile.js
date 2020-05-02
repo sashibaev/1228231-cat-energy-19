74fcee0294d738a3296b8dd32eb38c62f41ae1a3
Sergey Shibaev <srgjjshibaev@rambler.ru>
2020-05-02 20:10:17 +0300

    Оптимизируем изображения,создаем версии изображений в формате webp

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -18,11 +20,27 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-	.pipe(csso())
-	.pipe(rename("style.min.css"))
+  .pipe(csso())
+  .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png.jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+  ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+    return gulp.src("source/img/**/*.{png,jpg}")
+        .pipe(webp({quality: 90}))
+        .pipe(gulp.dest("source/img/webp"))
 });
 
 gulp.task("server", function () {
